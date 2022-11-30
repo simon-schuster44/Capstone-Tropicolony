14073cb9ee78465f5163110ae64373968a30b733
Simon Schuster <simschu@gmx.de>
2022-11-30 15:49:21 +0100

    applied requested changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import {Html, Head, Main, NextScript} from "next/document";
+import PalmtreeSvg from "../components/SVG/PalmtreeSvg";
 
 export default function Document() {
   return (
@@ -6,7 +7,7 @@ export default function Document() {
       <Head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐙</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏝</text></svg>"
         />
       </Head>
       <body>
