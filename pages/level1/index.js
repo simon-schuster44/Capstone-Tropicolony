d0aa838b016d26a92ad1cdecba798ef1f551a9b3
Simon Schuster <simschu@gmx.de>
2022-11-30 17:59:41 +0100

    added main menu

--- pages/level1/index.js
+++ pages/level1/index.js
@@ -2,14 +2,28 @@ import {useState} from "react";
 import Canvas from "../../components/Canvas";
 import {dataLevel1} from "../../components/Level1/_data";
 import Ressources from "../../components/Ressources/Ressources";
+import styled from "styled-components";
 
 export default function Level1() {
   const [array, setArray] = useState(dataLevel1);
 
   return (
     <>
+      <Background />
       <Canvas array={array} setArray={setArray} />
       <Ressources />
     </>
   );
 }
+
+const Background = styled.main`
+  position: absolute;
+  margin: 0;
+  top: 0;
+  background: url(/img/game-background.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+`;
