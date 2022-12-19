0c5389fdae7a1845f9b76b2074616840d10bb326
Simon Schuster <simschu@gmx.de>
2022-12-19 15:57:00 +0100

    local storage bugfix

--- components/useLocalStorage.js
+++ components/useLocalStorage.js
@@ -1,11 +1,37 @@
-import {useEffect, useState} from "react";
+//import {useEffect, useState} from "react";
 
-export default function useLocalStorage(initialValue, storageName) {
-  const [state, setState] = useState(() => {
-    return JSON.parse(localStorage.getItem(storageName)) ?? initialValue;
-  });
+// export default function useLocalStorage(initialValue, storageName) {
+//   const [state, setState] = useState(() => {
+//     return JSON.parse(localStorage.getItem(storageName)) ?? initialValue;
+//   });
+//   useEffect(() => {
+//     localStorage.setItem(storageName, JSON.stringify(state));
+//   }, [initialValue, state]);
+//   return [state, setState];
+// }
+
+import {useCallback, useEffect, useState} from "react";
+
+export function useLocalStorage(initialState, storageName) {
+  const [state, setState] = useState(initialState);
+  const setStateAndLocalStorage = useCallback(
+    callbackOrValue => {
+      setState(previousValue => {
+        const nextValue =
+          typeof callbackOrValue === "function"
+            ? callbackOrValue(previousValue)
+            : callbackOrValue;
+        window.localStorage.setItem(storageName, JSON.stringify(nextValue));
+        return nextValue;
+      });
+    },
+    [key]
+  );
   useEffect(() => {
-    localStorage.setItem(storageName, JSON.stringify(state));
-  }, [initialValue, state]);
-  return [state, setState];
+    const stored = window.localStorage.getItem(key);
+    if (stored !== null) {
+      setState(JSON.parse(stored));
+    }
+  }, [key]);
+  return [state, setStateAndLocalStorage];
 }
